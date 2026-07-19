b57554316c8616fe31ee56f6d68fedc7cd253b55
BUHARI SAIDU <207902399+MuhmdBuharisaeed@users.noreply.github.com>
2026-07-19 09:52:43 +0100

    Add asset-chart-box styles to CSS

---
+++ css/asset-chart-box.js
@@ -0,0 +1,33 @@
+.asset-chart-box{
+
+margin-top:12px;
+height:42px;
+display:flex;
+align-items:center;
+
+}
+
+.asset-chart{
+
+width:100%;
+height:40px;
+
+}
+
+.asset-footer{
+
+display:flex;
+justify-content:space-between;
+margin-top:8px;
+font-size:12px;
+opacity:.85;
+
+}
+
+.asset-footer b{
+
+display:block;
+font-size:14px;
+margin-top:2px;
+
+  }
