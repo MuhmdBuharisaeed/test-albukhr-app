b5d031327098f817fb0a6c82de673b5b025ca387
BUHARI SAIDU <207902399+MuhmdBuharisaeed@users.noreply.github.com>
2026-08-12 16:27:45 +0100

    Refactor supabase.js for improved formatting

--- js/supabase.js
+++ js/supabase.js
@@ -1,18 +1,13 @@
 const SUPABASE_URL =
-"https://qexmnghilahsvethlxem.supabase.co";
+  "https://qexmnghilahsvethlxem.supabase.co";
 
 const SUPABASE_KEY =
-"sb_publishable_mSbWlhVKdmSjasKJC50QYw_5wzgRMe2";
+  "sb_publishable_mSbWlhVKdmSjasKJC50QYw_5wzgRMe2";
 
 const supabaseClient =
-window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
 
 window.supabaseClient = supabaseClient;
-
-document.body.insertAdjacentHTML(
-  "beforeend",
-  `<pre>${JSON.stringify(Object.keys(window.supabase || {}), null, 2)}</pre>`
-);
