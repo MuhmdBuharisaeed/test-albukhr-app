735560dcaebef7524ab831d10f193f476348eb73
BUHARI SAIDU <207902399+MuhmdBuharisaeed@users.noreply.github.com>
2026-08-25 22:16:33 +0100

    Initialize Supabase client in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,13 @@
+const SUPABASE_URL =
+  "https://qexmnghilahsvethlxem.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_mSbWlhVKdmSjasKJC50QYw_5wzgRMe2";
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
+
+window.supabaseClient = supabaseClient;
